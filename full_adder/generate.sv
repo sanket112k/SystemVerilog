171c4ce2dc24fba10dc241d849f0af0179d57940
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-01-15 23:45:02 +0530

    Create generate.sv

---
+++ full_adder/generate.sv
@@ -0,0 +1,17 @@
+class generator;
+  transaction trans;
+  mailbox gen2drv;
+  
+  function new(mailbox gen2drv);
+    this.gen2drv = gen2drv;
+  endfunction
+  
+  task main();
+    repeat (2) begin
+      trans = new();
+      trans.randamize();
+      trans.display("generator class signals");
+      gen2drv.put(trans);
+    end
+  endtask
+endclass
