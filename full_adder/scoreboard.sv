665826cd3f78266738427ca44c857af8ec0012cc
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-01-16 00:07:54 +0530

    Update scoreboard.sv

--- full_adder/scoreboard.sv
+++ full_adder/scoreboard.sv
@@ -1,1 +1,21 @@
-//scoreboard
+class scoreboard;
+  mailbox mon2scb;
+  
+  function new(mailbox mon2scb);
+    this.mon2scb = mon2scb;
+  endfunction
+  
+  task main();
+    transaction trans;
+    repeat(2) begin
+      mon2scb.get(trans);
+      trans.display("scoreboard signals");
+      
+      if(((trans.a ^ trans.b ^ trans.c) == trans.sum) && (((trans.a & trans.b) | (trans.b & trans.c) | (trans.c & trans.a)) == trans.cout)) $display ("________PASS________");
+      else $display ("________FAIL________");
+      
+      $display("________TRANSACTION DONE________");
+      $display("\n");
+    end
+  endtask
+endclass
