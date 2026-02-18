5eb11a03928ffb96c1a2b83a9ca6873b6e7edd81
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-02-18 16:02:12 +0530

    Create data_type_casting.sv

---
+++ data_type_casting.sv
@@ -0,0 +1,26 @@
+module casting_shortint();
+ shortint shortint_data;
+ int int_data;
+ longint longint_data;
+
+ initial begin
+   shortint_data = 8'b10110011;
+
+   $display("\nsize of shortint = %0d",$size(shortint_data));
+   $display("before casting shortint_data = %b",shortint_data);
+
+   int_data = int'(shortint_data);  
+
+   $display("after casting shortint to int = %b",int_data);
+
+   longint_data = longint'(shortint_data); 
+
+   $display("after casting shortint to longint = %b\n",longint_data);
+ end
+endmodule
+/*
+OUTPUT:
+before casting shortint_data = 0000000010110011
+after casting shortint to int = 00000000000000000000000010110011
+after casting shortint to longint = 0000000000000000000000000000000000000000000000000000000010110011
+*/
