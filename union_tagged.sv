0b24fd378e2cc18bd58b425ac7ab68a3f27ef3ca
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-02-19 18:47:12 +0530

    Create union_tagged.sv

---
+++ union_tagged.sv
@@ -0,0 +1,33 @@
+typedef union tagged {
+  int i_data;
+  byte b_data;
+  logic [3:0] nibble;
+} my_union_t;
+
+module tagged_union_example;
+  my_union_t u;
+  initial begin
+    u = tagged i_data 100;
+    if (u matches tagged i_data)
+      $display("Integer value = %0d", u.i_data);
+
+    u = tagged b_data 8'hAA;
+    if (u matches tagged i_data)
+      $display("Integer = %0d", u.i_data);
+    else if (u matches tagged b_data)
+      $display("Byte = %0h", u.b_data);
+    else if (u matches tagged nibble)
+      $display("Nibble = %0h", u.nibble);
+    
+    u = tagged nibble 4'hF;
+    if (u matches tagged nibble)
+      $display("Nibble value = %0h", u.nibble);
+  end
+endmodule
+
+/*
+OUTPUT:
+Integer value = 100
+Byte = aa
+Nibble value = f
+*/
