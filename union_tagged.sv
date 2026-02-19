2a3094fea17ea8fc5302f5376bd8b85ad443e3c1
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-02-19 18:53:32 +0530

    Update union_tagged.sv

--- union_tagged.sv
+++ union_tagged.sv
@@ -8,26 +8,27 @@ module tagged_union_example;
   my_union_t u;
   initial begin
     u = tagged i_data 100;
-    if (u matches tagged i_data)
-      $display("Integer value = %0d", u.i_data);
-
+    if (u matches tagged i_data) $display("Integer value = %0d", u.i_data);
+    $display("my_union_t = %p", u);
+    
     u = tagged b_data 8'hAA;
-    if (u matches tagged i_data)
-      $display("Integer = %0d", u.i_data);
-    else if (u matches tagged b_data)
-      $display("Byte = %0h", u.b_data);
-    else if (u matches tagged nibble)
-      $display("Nibble = %0h", u.nibble);
+    if (u matches tagged i_data) $display("Integer = %0d", u.i_data);
+    else if (u matches tagged b_data) $display("Byte = %0h", u.b_data);
+    else if (u matches tagged nibble) $display("Nibble = %0h", u.nibble);
+    $display("my_union_t = %p", u);
     
     u = tagged nibble 4'hF;
-    if (u matches tagged nibble)
-      $display("Nibble value = %0h", u.nibble);
+    if (u matches tagged nibble) $display("Nibble value = %0h", u.nibble);
+    $display("my_union_t = %p", u);
   end
 endmodule
 
 /*
 OUTPUT:
 Integer value = 100
-Byte = aa
-Nibble value = f
+# my_union_t = '{tagged i_data:100}
+# Byte = aa
+# my_union_t = '{tagged b_data:-86}
+# Nibble value = f
+# my_union_t = '{tagged nibble:15}
 */
