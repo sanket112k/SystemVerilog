8538982bab79a70a992e1c220c566507dfea6627
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-17 00:39:03 +0530

    Add agent class with generator, driver, and monitor

---
+++ full_adder_with agent/tb/agent.sv
@@ -0,0 +1,25 @@
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+
+class agent;
+
+  generator gen;
+  driver drv;
+  monitor mon;
+
+  mailbox #(transaction) gen2drv;
+  mailbox #(transaction) mon2scb;
+
+  function new(virtual fa_if vif);
+
+    gen2drv = new();
+    mon2scb = new();
+
+    gen = new(gen2drv);
+    drv = new(vif, gen2drv);
+    mon = new(vif, mon2scb);
+
+  endfunction
+
+endclass
