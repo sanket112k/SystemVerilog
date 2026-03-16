80f0ef5fde9f9354efb88bb69242718a01b21e1e
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-17 00:38:13 +0530

    Add scoreboard class for full adder verification

---
+++ full_adder_with agent/tb/scoreboard.sv
@@ -0,0 +1,27 @@
+class scoreboard;
+
+  mailbox #(transaction) mon2scb;
+  transaction tr;
+
+  function new(mailbox #(transaction) mon2scb);
+    this.mon2scb = mon2scb;
+  endfunction
+
+  task run();
+    bit exp_sum;
+    bit exp_cout;
+    
+    forever begin
+      mon2scb.get(tr);
+
+      exp_sum  = tr.a ^ tr.b ^ tr.cin;
+      exp_cout = (tr.a & tr.b) | (tr.b & tr.cin) | (tr.a & tr.cin);
+
+      if(tr.sum == exp_sum && tr.cout == exp_cout)
+        $display("PASS");
+      else
+        $display("FAIL");
+    end
+  endtask
+
+endclass
