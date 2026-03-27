2762c3c89e530cb9b4c3579321a449287d9e0b21
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-27 23:26:35 +0530

    Refactor environment class to use agent

--- D flip flop/environment.sv
+++ D flip flop/environment.sv
@@ -1,40 +1,30 @@
 `include "transaction.sv"
-`include "generator.sv"
-`include "driver.sv"
-`include "monitor.sv"
+`include "agent.sv"
 `include "scoreboard.sv"
 
 class environment;
-  generator gen;
-  driver drv;
-  monitor mon;
+
+  agent agt;
   scoreboard scb;
-  
-  mailbox #(transaction) gen2drv;
-  mailbox #(transaction) mon2scb;
-  
-  virtual intf vif;
-  function new (virtual intf vif);
-    this.vif = vif;
-    gen2drv = new();
-    mon2scb = new();
-  	gen = new(gen2drv);
-  	drv = new(gen2drv);
-  	mon = new(mon2scb);
-  	scb = new(mon2scb);
+
+  function new(virtual dff_if vif);
+    agt = new(vif);
+    scb = new(agt.mon2scb, 20);
   endfunction
-  
-  task test_run();
+
+  task run();
+
     fork
-      gen.run();
-      drv.run();
-      mon.run();
+      agt.gen.run();
+      agt.drv.run();
+      agt.mon.run();
       scb.run();
     join_none
-    
-    wait(gen.num_transactions == 0);
-    #10;
+
+    @(scb.done);
     scb.report();
     $finish;
+
   endtask
+
 endclass
