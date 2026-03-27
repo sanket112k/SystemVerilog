45177ed245675d77a525edc049d69beba20f83a5
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-27 23:25:58 +0530

    Refactor scoreboard class for improved tracking
    Refactor scoreboard class to track pass and fail counts more effectively. Update report function to display pass and fail counts based on transactions.

--- D flip flop/scoreboard.sv
+++ D flip flop/scoreboard.sv
@@ -1,37 +1,45 @@
 class scoreboard;
-  
+  int expected_count;
+  event done;
+
   mailbox #(transaction) mon2scb;
-  int pass_count = 0;
-  int fail_count = 0;
-  
-  function new(mailbox #(transaction) mon2scb);
+  int pass, fail;
+
+  function new(mailbox #(transaction) mon2scb, int expected_count);
     this.mon2scb = mon2scb;
+    this.expected_count = expected_count;
   endfunction
-  
+
   task run();
-    transaction actual_tr;
-    bit expected_q;
-    
+    transaction tr;
+    bit expected;
+
     forever begin
-      mon2scb.get(actual_tr);
-      
-      if(actual_tr.reset == 0) expected_q = 0;
-      else expected_q = actual_tr.data;
-      if (actual_tr.q == expected_q) begin
-        $display("[SCOREBOARD] T=%0t: PASS! D=%0h, Reset=%0h, Q_actual=%0h, Q_expected=%0h", $time, actual_tr.data, actual_tr.reset, actual_tr.q, expected_q);
-        pass_count++;
+      mon2scb.get(tr);
+
+      if (tr.reset)
+        expected = 0;
+      else
+        expected = tr.d;
+
+      if (tr.q === expected) begin
+        pass++;
+        $display("[PASS] d=%0b reset=%0b q=%0b",
+                  tr.d, tr.reset, tr.q);
       end else begin
-        $display("[SCOREBOARD] T=%0t: FAIL! D=%0h, Reset=%0h, Q_actual=%0h, Q_expected=%0h", $time, actual_tr.data, actual_tr.reset, actual_tr.q, expected_q);
-        fail_count++;
+        fail++;
+        $display("[FAIL] d=%0b reset=%0b q=%0b expected=%0b",
+                  tr.d, tr.reset, tr.q, expected);
       end
+      if ((pass + fail) == expected_count)
+      ->done;   // signal completion
     end
   endtask
-  
+
   function void report();
-    $display("-------------------------------------");
-    $display("Scoreboard Report:");
-    $display("Total Transactions: %0d", pass_count + fail_count);
-    $display("Passes: %0d", pass_count);
-    $display("failure: %0d", fail_count);
+    $display("\n==== REPORT ====");
+    $display("PASS = %0d", pass);
+    $display("FAIL = %0d", fail);
   endfunction
+
 endclass
