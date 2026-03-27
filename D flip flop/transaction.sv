2b1a937fd2b4cece0932d11cd84d5331bb4328d1
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-27 23:23:24 +0530

    Refactor transaction class for signal naming
    Updated transaction class to use 'd' instead of 'data' and modified display function.

--- D flip flop/transaction.sv
+++ D flip flop/transaction.sv
@@ -1,14 +1,9 @@
 class transaction;
-  rand bit data;
+  rand bit d;
   rand bit reset;
   bit q;
-  
-  function void calc_expeted(bit current_q);
-    if (reset == 0) q = 0;
-    else q = data;
-  endfunction
-  
-  function void display(string name = "");
-    $display("[%0t] %s: data=%0b, reset=%0b, q_expected=%0b", $time, name, data, reset, q);
+
+  function void display(string tag);
+    $display("[%0t] %s: d=%0b reset=%0b q=%0b", $time, tag, d, reset, q);
   endfunction
 endclass
