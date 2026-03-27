707eef40ada3a2555dacdf6b8b2a4bbe4c832fec
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-27 23:22:09 +0530

    Refactor testbench module for D flip flop

--- D flip flop/testbench.sv
+++ D flip flop/testbench.sv
@@ -1,20 +1,30 @@
 `include "interface.sv"
 `include "test.sv"
 
-module testbench;
-  intf intff();
-  test tst(intff);
-  
-  dff_async_reset ff(
-    .clk(intff.clk),
-    .reset(intff.reset),
-    .data(intff.data),
-    .q(intff.q)
+module tb;
+
+  logic clk = 0;
+  always #5 clk = ~clk;
+
+  dff_if vif(clk);
+
+  dff dut (
+    .clk   (clk),
+    .reset (vif.reset),
+    .d     (vif.d),
+    .q     (vif.q)
   );
+
+  test t;
+
+  initial begin
+    t = new(vif);
+    t.run();
+  end
   
   initial begin
     $dumpfile("testbench.vcd");
     $dumpvars;
   end
-  
+
 endmodule
