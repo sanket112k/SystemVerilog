d064aadc96f72ec0d404df51bfffc7cbd61e5fcb
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-27 23:24:41 +0530

    Refactor driver class to use virtual dff_if interface

--- D flip flop/driver.sv
+++ D flip flop/driver.sv
@@ -1,20 +1,25 @@
 class driver;
-  virtual intf vif;
+
+  virtual dff_if vif;
   mailbox #(transaction) gen2drv;
-  
-  function new(mailbox #(transaction) gen2drv);
+
+  function new(virtual dff_if vif,
+               mailbox #(transaction) gen2drv);
+    this.vif = vif;
     this.gen2drv = gen2drv;
   endfunction
-  
+
   task run();
     transaction tr;
+
     forever begin
       gen2drv.get(tr);
-      
-      @(posedge vif.clk);
-      vif.data <= tr.data;
-      vif.reset <= tr.reset;
-      tr.display("Driver class signals");
+
+      vif.cb.d     <= tr.d;
+      vif.cb.reset <= tr.reset;
+
+      @(vif.cb);
     end
   endtask
+
 endclass
