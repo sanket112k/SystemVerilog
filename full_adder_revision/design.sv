83d6acc779352771daee8c8a6d891815a5e64784
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-30 18:12:01 +0530

    Create design.sv

---
+++ full_adder_revision/design.sv
@@ -0,0 +1,15 @@
+module full_adder(
+    input a, b, cin,
+    output sum, cout
+);
+wire w1, w2, w3;
+
+xor x1(sum, a, b, cin);
+
+and a1(w1, a, b);
+and a2(w2, b, cin);
+and a3(w3, cin, a);
+
+or o1(cout, w1, w2, w3);
+
+endmodule
