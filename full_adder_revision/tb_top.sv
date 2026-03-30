53e4ffc87b004b61dc00d679a34edb85544c71c9
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-30 18:20:02 +0530

    Add files via upload

---
+++ full_adder_revision/tb_top.sv
@@ -0,0 +1,25 @@
+`include "interface.sv"
+`include "test.sv"
+
+module tb;
+logic clk;
+always #5 clk = ~clk;
+
+fa_if vif(clk);
+
+full_adder dut(
+    .a(vif.a),
+    .b(vif.b),
+    .cin(vif.cin),
+    .sum(vif.sum),
+    .cout(vif.cout)
+);
+
+test t;
+
+initial begin
+    t = new(vif);
+    t. run();
+    #100 finish;
+end
+endmodule
