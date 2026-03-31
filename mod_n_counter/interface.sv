3f2b6fa4c8dcaa8b9d1509cdd4250ddd92210ef4
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-31 15:06:26 +0530

    Add SystemVerilog interface for counting

---
+++ mod_n_counter/interface.sv
@@ -0,0 +1,11 @@
+`import tb_pkg::*;
+
+interface count_if(input logic clk);
+  logic reset;
+  logic [$clog2(N)-1 : 0] count;
+  
+  clocking cb @(posedge clk);
+    default input #1step output #0;
+    input count;
+    output reset;
+endinterface
