57399469f287db4b477475ea259dca2b008997cf
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-05 00:02:43 +0530

    Update tb_top.sv

--- mod_n_counter/tb_top.sv
+++ mod_n_counter/tb_top.sv
@@ -1,3 +1,5 @@
+`include "tb_pkg.sv"
+import tb_pkg::*;
 `include "interface.sv"
 `include "test.sv"
 
@@ -5,13 +7,24 @@ module tb;
   logic clk = 0;
   always #5 clk = ~clk;
   
-  dff_if vif (clk);
+  count_if vif (clk);
   
-  dff dut #(
+  mod_n_counter #(
     .N(N)
-  )(
+  ) dut(
     .clk(clk),
     .reset(vif.reset),
-    .count(vif.q)
+    .count(vif.count)
   );
   test t;
+  
+  initial begin
+    t = new(vif);
+    t.run;
+  end
+  
+  initial begin
+    $dumpfile("testbench.vcd");
+    $dumpvars;
+  end
+endmodule
