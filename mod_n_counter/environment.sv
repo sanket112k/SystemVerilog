f05f8caca8be98b59fe4741a1dc691260f8860ea
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-05 00:00:32 +0530

    Create environment.sv

---
+++ mod_n_counter/environment.sv
@@ -0,0 +1,30 @@
+`include "transaction.sv"
+`include "agent.sv"
+`include "scoreboard.sv"
+
+class environment;
+
+  agent agt;
+  scoreboard scb;
+
+  function new(virtual count_if vif);
+    agt = new(vif);
+    scb = new(agt.mon2scb, 20);
+  endfunction
+
+  task run();
+
+    fork
+      agt.gen.run();
+      agt.drv.run();
+      agt.mon.run();
+      scb.run();
+    join_none
+
+    @(scb.done);
+    scb.report();
+    $finish;
+
+  endtask
+
+endclass
