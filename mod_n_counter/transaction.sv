8f0564b2b3e37c7f15955f62a0faadc6b0847a7c
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-06 18:00:13 +0530

    Update transaction.sv

--- mod_n_counter/transaction.sv
+++ mod_n_counter/transaction.sv
@@ -1,9 +1,12 @@
-//`include "tb_pkg.sv"
 import tb_pkg::*;
 
 class transaction;
   rand bit reset;
   bit [$clog2(N)-1 : 0] count;
+  
+  constraint r_var {
+    reset dist{1 := 20, 0 := 80};
+  }
   
   function void display(string tag);
     $display("[%0t] %s: reset=%0b count=%0b", $time, tag, reset, count);
