2c58637a9b5cf605ecf322fd991b2645c1db7a3c
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-07 23:42:44 +0530

    Add transaction class for memory operations

---
+++ single_port_sync_ram/transaction.sv
@@ -0,0 +1,22 @@
+import tb_pkg::*;
+
+class transaction;
+  rand bit [ADDR_WIDTH-1 : 0] addr;
+  rand bit cs;
+  rand bit we;
+  rand bit oe;
+  rand bit [DATA_WIDTH-1 : 0] data;
+  bit [DATA_WIDTH-1 : 0] rdata;
+  
+  constraint valid{
+  cs == 1;	// always access memory
+  
+  // Only one operation at a time
+  if (we) oe == 0;
+  else oe == 1;
+  }
+  
+  function void display(string name);
+    $display("[%s] addr=%0d cs=%0b we=%0b oe=%0b data=%0h rdata=%0h", name, addr, cs, we, oe, data, rdata);
+  endfunction
+endclass
