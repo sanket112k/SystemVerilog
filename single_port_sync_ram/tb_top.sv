f9068ac5cb8038928dee114a649b07bdd25ac679
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-07 23:41:18 +0530

    Add testbench for single port synchronous RAM

---
+++ single_port_sync_ram/tb_top.sv
@@ -0,0 +1,36 @@
+`include "tb_pkg.sv"
+`include "interface.sv"
+`include "test.sv"
+
+module tb_top;
+  import tb_pkg::*;
+  
+  logic clk;
+  always #5 clk = ~clk;
+  
+  ram_if vif(clk);
+  single_port_sync_ram #(
+    .DEPTH(DEPTH),
+    .DATA_WIDTH(DATA_WIDTH)
+  ) dut(
+    .clk(vif.clk),
+    .addr(vif.addr),
+    .cs(vif.cs),
+    .we(vif.we),
+    .oe(vif.oe),
+    .data(vif.data)
+  );
+  
+  test t;
+  
+  initial begin
+    t = new(vif);
+    t.run();
+  end
+  
+  initial begin
+    $dumpfile("testbench.vcd");
+    $dumpvars;
+  end
+endmodule
+  
