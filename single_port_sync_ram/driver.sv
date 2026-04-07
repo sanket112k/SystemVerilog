e8c57e12f39fcaf84191cff6d4c46979dd17004b
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-07 23:44:07 +0530

    Add driver class for single port synchronous RAM

---
+++ single_port_sync_ram/driver.sv
@@ -0,0 +1,32 @@
+class driver;
+  virtual ram_if vif;
+  mailbox #(transaction) gen2drv;
+  
+  function new(virtual ram_if vif, mailbox #(transaction) gen2drv);
+    this.vif = vif;
+    this.gen2drv = gen2drv;
+  endfunction
+  
+  task run();
+    transaction tr;
+    
+    forever begin
+      $display("[DRV] Waiting for transaction");
+      gen2drv.get(tr);
+      $display("[DRV] Got transaction");
+      
+      vif.cb.addr <= tr.addr;
+      vif.cb.cs <= tr.cs;
+      vif.cb.we <= tr.we;
+      vif.cb.oe <= tr.oe;
+      if (tr.we)
+        vif.cb.data <= tr.data;
+      else
+        vif.cb.data <= 'bz;
+      $display("[DRV] Converted to signal level");
+      
+      @(vif.cb);
+      tr.display("DRV");
+    end
+  endtask
+endclass
