62d406984f33e0f470a0e9d672dc54b2ade79f92
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-10 21:47:33 +0530

    Create fifo_mem.sv

---
+++ Asynchronous FIFO/rtl/fifo_mem.sv
@@ -0,0 +1,24 @@
+// FIFO memory buffer
+module fifo_mem #(
+  parameter DEPTH = 8,
+  parameter DATA_WIDTH = 16,
+  localparam ADDR_WIDTH = $clog2(DEPTH)
+)(
+  input  logic                    wclk,
+  input  logic                    wen,
+  input  logic [ADDR_WIDTH-1 : 0] waddr;
+  input  logic [DATA_WIDTH-1 : 0] wdata,
+  
+  input  logic                    rclk,
+  input  logic                    ren,
+  input  logic [ADDR_WIDTH-1 : 0] raddr,
+  output logic [DATA_WIDTH-1 : 0] rdata
+);
+  logic [DATA_WIDTH-1 : 0] mem [0 : DEPTH-1];
+  
+  always_ff @(posedge wclk)
+    if (wen) mem[waddr] <= wdata;
+  
+  always_ff @(posedge rclk)
+    if (ren) rdata <= mem[raddr];
+endmodule
