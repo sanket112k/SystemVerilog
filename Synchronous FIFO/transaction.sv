b90691c78ab98e979adb32679156db7cee1bd314
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-10 15:07:47 +0530

    Remove resetn constraint from transaction class
    Removed constraint for resetn in transaction class.

--- Synchronous FIFO/transaction.sv
+++ Synchronous FIFO/transaction.sv
@@ -7,8 +7,6 @@ class transaction;
   bit full;
   bit empty;
   
-  //constraint a {resetn dist {1'b0 := 95, 1'b1 := 5}};
-  
   function void display(string name);
     $display("[%0t] %0s:        resetn=%0b w_en=%0b r_en=%0b data_in=%0h data_out=%0h full=%0b empty=%0b", $time, name, resetn, w_en, r_en, data_in, data_out, full, empty);
   endfunction
