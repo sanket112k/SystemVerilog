190c9ce57e0e747d29515f3580ef8feb40f8cb8c
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-11 23:57:04 +0530

    Create fifo_write_if.sv

---
+++ Asynchronous FIFO/tb/fifo_write_if.sv
@@ -0,0 +1,28 @@
+interface fifo_if(input logic wclk);
+  import tb_pkg::*;
+  
+  logic 				   wreset;
+  logic 				   wen;
+  logic [DATA_WIDTH-1 : 0] wdata;
+  logic 				   full;
+  
+  clocking w_drv_cb @(posedge wclk);
+    default input #1step output #0;
+    output wreset, wen, wdata;
+    input full;
+  endclocking
+  
+  clocking w_mon_cb @(posedge wclk);
+    default input #0;
+    input wreset, wen, wdata;
+    input full;
+  endclocking
+  
+  modport w_drv_mp (clocking w_drv_cb, import task wait_clocks(int n));
+  modport w_mon_mp (clocking w_mon_cb);
+  modport dut_mp   (input wreset, wen, wdata, input full);
+  
+  task automatic wait_clock(int n);
+    repeat(n) @(posedge wclk);
+  endtask
+endinterface
