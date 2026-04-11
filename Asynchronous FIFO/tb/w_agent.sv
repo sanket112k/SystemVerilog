fb5ffb5b2953cf2722184ce85963c346889a841f
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-11 23:59:28 +0530

    Create w_agent.sv

---
+++ Asynchronous FIFO/tb/w_agent.sv
@@ -0,0 +1,22 @@
+`include "w_generator.sv"
+`include "w_driver.sv"
+`include "w_monitor.sv"
+
+class w_agent;
+  
+  mailbox #(w_transaction) wgen2drv;
+  mailbox #(w_transaction) wmon2scb;
+  
+  w_generator wgen;
+  w_driver    wdrv;
+  w_monitor   wmon;
+  
+  function new(virtual fifo_write_if wvif);
+    wgen2drv = new();
+    wmon2scb = new();
+    
+    wgen = new(wgen2drv);
+    wdrv = new(wvif, wgen2drv);
+    wmon = new(wvif, wmon2scb);
+  endfunction
+endclass
