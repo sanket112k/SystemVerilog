272e557fa6b815b59f22248d454533d7e8c39335
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-12 00:00:21 +0530

    Create r_driver.sv

---
+++ Asynchronous FIFO/tb/r_driver.sv
@@ -0,0 +1,23 @@
+class r_driver;
+  
+  r_transaction wtr;
+  virtual fifo_read_if rvif;
+  mailbox #(r_transaction) rgen2drv;
+  
+  function new(virtual fifo_read_if rvif, mailbox #(r_transaction) rgen2drv);
+    this.rvif = rvif;
+    this.rgen2drv = rgen2drv;
+  endfunction
+  
+  task run();
+    forever begin
+      rgen2drv.get(rtr);
+      rtr.display("RDRV");
+      
+      @(rvif.r_drv_cb);
+      
+      rvif.r_drv_cb.rreset <= rtr.rreset;
+      rvif.r_drv_cb.ren    <= rtr.ren;
+    end
+  endtask
+endclass
