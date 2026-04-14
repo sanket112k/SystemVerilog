4d756d7bce5caeebe1d6431441de69816607cd29
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-14 23:41:51 +0530

    Update scoreboard.sv

--- Asynchronous FIFO/tb/scoreboard.sv
+++ Asynchronous FIFO/tb/scoreboard.sv
@@ -22,6 +22,41 @@ class scoreboard;
   endfunction
   
   task run();
+    fork
+      forever begin		// Thread 1: collect all writes into reference queue
+        wmon2scb.get(wtr);
+        if(!wtr.wreset && wtr.wen && !wtr.full)
+          ref_queue.push_back(wtr.wdata);
+      end
+      
+      forever begin		// Thread 2: check reads against reference queue
+        rmon2scb.get(rtr);
+        if(!rtr.rreset && rtr.rvalid) begin
+          if(ref_queue.size() == 0) begin
+            $display("[SCB] FAIL: rvalid seen but ref_queue is empty");
+            fail++;
+          end
+          else begin
+            exp_rdata = ref_queue.pop_front();
+            if (rtr.rdata === exp_rdata) begin
+              $display("[%0t] SCB: {PASS} wreset=%0b wen=%0b wdata=%0h full=%0b rreset=%0b ren=%0b rdata=%0h empty=%0b rvalid=%0b", $time, wtr.wreset, wtr.wen, wtr.wdata, wtr.full, rtr.rreset, rtr.ren, rtr.rdata, rtr.empty, rtr.rvalid);
+              pass++;
+            end
+            else begin
+              $display("[%0t] SCB: {FAIL} wreset=%0b wen=%0b wdata=%0h full=%0b rreset=%0b ren=%0b rdata=%0h empty=%0b rvalid=%0b | exp_data_out=%0h exp_full=%0b exp_empty=%0b", $time, wtr.wreset, wtr.wen, wtr.wdata, wtr.full, rtr.rreset, rtr.ren, rtr.rdata, rtr.empty, rtr.rvalid, exp_rdata, exp_full, exp_empty);
+              fail++;
+            end
+          end
+          $display("--------------------------------------------");
+          if(pass + fail == (2*DEPTH + 10))
+            -> done;
+        end
+      end
+    join_none
+  endtask
+    
+    
+    /*
     forever begin
       wmon2scb.get(wtr);
       rmon2scb.get(rtr);
@@ -58,6 +93,7 @@ class scoreboard;
         -> done;
     end
   endtask
+  */
   
   function void report();
     $display("\n==== REPORT ====");
