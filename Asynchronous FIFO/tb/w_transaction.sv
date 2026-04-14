7a8c0c53f7374eaaf9e81197741ff8f7c8edbe17
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-14 23:43:03 +0530

    Update w_transaction.sv

--- Asynchronous FIFO/tb/w_transaction.sv
+++ Asynchronous FIFO/tb/w_transaction.sv
@@ -6,6 +6,20 @@ class w_transaction;
   rand bit [DATA_WIDTH-1 : 0] wdata;
   bit 						  full;
   
+  rand int unsigned 		  delay_cycles;
+  longint 					  wclk_time;	// $time stamp in write clock domain
+  
+  // Deep copy
+  function w_transaction clone();
+    clone = new();
+    clone.wreset		= this.wreset;
+    clone.wen 			= this.wen;
+    clone.wdata 		= this.wdata;
+    clone.full 			= this.full;
+    clone.delay_cycles 	= this.delay_cycles;
+    clone.wclk_time		= this.wclk_time;
+  endfunction
+  
   function void display(string name);
     $display("[%0t] %0s:        wreset=%0b wen=%0b wdata=%0h full=%0b", $time, name, wreset, wen, wdata, full);
   endfunction
