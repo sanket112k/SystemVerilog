18c7c55d92cade2c194ae73c6ce68dc040ce1a65
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-04-17 22:41:34 +0530

    Update r_generator.sv

--- Asynchronous FIFO/tb/r_generator.sv
+++ Asynchronous FIFO/tb/r_generator.sv
@@ -4,6 +4,7 @@ class r_generator;
   
   r_transaction rtr;
   mailbox #(r_transaction) rgen2drv;
+  bit rdone;
   
   function new(mailbox #(r_transaction) rgen2drv);
     this.rgen2drv = rgen2drv;
@@ -14,28 +15,29 @@ class r_generator;
       rtr = new();
       assert(rtr.randomize() with {
         rreset == 1'b1;
+        rdone  == 1'b0;
       });
-      rgen2drv.put(rtr);
+      rgen2drv.put(rtr.clone());
       rtr.display("RGEN");
     end
     
-    repeat(DEPTH) begin 	// Don't read
+    repeat(10) begin 	// Don't read
       rtr = new();
       assert(rtr.randomize() with {
         rreset == 1'b0;
         ren    == 1'b0;
       });
-      rgen2drv.put(rtr);
+      rgen2drv.put(rtr.clone());
       rtr.display("RGEN");
     end
     
-    repeat(DEPTH) begin 	// read
+    repeat(10) begin 	// read
       rtr = new();
       assert(rtr.randomize() with {
         rreset == 1'b0;
         ren    == 1'b1;
       });
-      rgen2drv.put(rtr);
+      rgen2drv.put(rtr.clone());
       rtr.display("RGEN");
     end
     
@@ -45,9 +47,20 @@ class r_generator;
         rreset == 1'b0;
         ren    == 1'b1;
       });
-      rgen2drv.put(rtr);
+      rgen2drv.put(rtr.clone());
       rtr.display("RGEN");
     end
     
+    begin 		// Both write and read
+      rtr = new();
+      assert(rtr.randomize() with {
+        rreset == 1'b0;
+        ren    == 1'b1;
+      });
+      rgen2drv.put(rtr.clone());
+      rtr.display("RGEN");
+    end
+    
+    rdone = 1'b1;
   endtask
 endclass
