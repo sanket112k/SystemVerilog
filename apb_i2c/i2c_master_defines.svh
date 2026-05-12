e73bdb8ad8a848bb11b7dacc52de0b7b7b23f587
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-12 23:27:06 +0530

    Add I2C command definitions in i2c_master_defines.svh

---
+++ apb_i2c/i2c_master_defines.svh
@@ -0,0 +1,11 @@
+`ifndef I2C_CMD_VH
+`define I2C_CMD_VH
+
+// bitcontroller states
+`define I2C_CMD_NOP   3'h0
+`define I2C_CMD_START 3'h1
+`define I2C_CMD_STOP  3'h2
+`define I2C_CMD_WRITE 3'h3
+`define I2C_CMD_READ  3'h4
+
+`endif
