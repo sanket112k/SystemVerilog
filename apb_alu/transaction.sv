337030e1e26ed9cf86f0b6f573d460a48c396875
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-16 23:53:51 +0530

    Refactor transaction classes and add copy methods

--- apb_alu/transaction.sv
+++ apb_alu/transaction.sv
@@ -1,15 +1,52 @@
-class transaction;
-  rand bit 			presetn;
-  rand bit 			psel;
-  rand bit 			penable;
-  rand bit 			pwrite;
-  rand bit [AW-1:0] paddr;
-  rand bit [DW-1:0] pwdata;
-       bit [DW-1:0] prdata;
-       bit 			pready;
-       bit 			pslverr;
+class apb_transaction;
+  bit 		   pwrite;
+  bit [AW-1:0] paddr;
+  bit [DW-1:0] pwdata;
+  bit [DW-1:0] prdata;
+  bit 		   pslverr;
   
-  function void display(string name);
-    $display();
+  function void copy(trasaction tr);
+    paddr   = tr.paddr  ;
+    pwrite  = tr.pwrite ;
+    pwdata  = tr.pwdata ;
+    prdata  = tr.prdata ;
+    pslverr = tr.pslverr;
+  endfunction
+  
+endclass
+
+class alu_in_transaction;
+  bit [DW-1:0] op_a  ;
+  bit [DW-1:0] op_b  ;
+  bit [3:0]    opcode;
+  
+  function new(bit [DW-1:0] op_a, bit [DW-1:0] op_b, bit [3:0]    opcode);
+    this.op_a   = op_a  ;
+    this.op_b   = op_b  ;
+    this.opcode = opcode;
+  endfunction
+  
+  function void copy(alu_in_transaction tr);
+    op_a   = tr.op_a  ;
+    op_b   = tr.op_b  ;
+    opcode = tr.opcode;
+  endfunction
+endclass
+
+class alu_out_transaction;
+  bit [DW-1:0] result;
+  bit [3:0]    flags;
+  bit          opc_illegal;
+  
+  function new();
+    result      = 0;
+    flags       = 0;
+    err_illegal = 0;
+  endfunction
+  
+  function void copy(alu_out_transaction tr);
+    result      = tr.result     ;
+    flags       = tr.flags      ;
+    err_illegal = tr.err_illegal;
   endfunction
 endclass
