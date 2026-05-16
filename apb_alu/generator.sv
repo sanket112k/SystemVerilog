e8a5d9dc646c0fa0058ec4034f9163807cd83891
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-16 23:54:38 +0530

    Implement generator class with constraints
    Define a generator class with constraints and a run task.

--- apb_alu/generator.sv
+++ apb_alu/generator.sv
@@ -1,3 +1,38 @@
+class generator;
+  mailbox gen2drv;
+  int num_items = 100;
+  int seed      = 1;
+  bit directed  = 0;	// 0 = random; 1 = smoke
+  int quota     = 20;
+  rand bit [DW-1:0] op_a;
+  rand bit [DW-1:0] op_b;
+  rand bit [3:0]    opcode;
+  
+  constraint c_opcode_legal{
+    opcode inside {[0:9]};
+  }
+  
+  constraint c_opcode_weighted{
+    opcode inside {4'h0:=25,
+                   4'h1:=25
+                   [4'h2:4'h4]:=10,
+                   [4'h5:4'h7]:=15,
+                   [4'h8:4'h9]:=10};
+  }
+  
+  function new(mailbox gen2drv);
+    this.gen2drv = gen2drv;
+  endfunction
+  
+  task run();
+    
+  endtask
+  
+endclass
+
+
+
+/*
 import tb_pkg::*;
 
 class generator;
@@ -23,3 +58,4 @@ class generator;
     $display("==========================================");
   endtask
 endclass
+*/
