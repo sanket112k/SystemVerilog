082c15c1910cecc429773dc1f7efa018bbc31097
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-16 23:53:10 +0530

    Add presetn input to apb_alu_if interface

--- apb_alu/apb_alu_if.sv
+++ apb_alu/apb_alu_if.sv
@@ -1,27 +1,26 @@
-interface apb_alu_if(input logic pclk);
+interface apb_alu_if(input logic pclk, input logic presetn);
   import tb_pkg::*;
   
-  logic 		 presetn,
-  logic 		 psel,
-  logic 		 penable,
-  logic 		 pwrite,
-  logic [AW-1:0] paddr,
-  logic [DW-1:0] pwdata,
-  logic [DW-1:0] prdata,
-  logic 		 pready,
-  logic 		 pslverr,
+  logic 		 psel;
+  logic 		 penable;
+  logic 		 pwrite;
+  logic [AW-1:0] paddr;
+  logic [DW-1:0] pwdata;
+  logic [DW-1:0] prdata;
+  logic 		 pready;
+  logic 		 pslverr;
   
   clocking drv_cb @(posedge clk);
     default input #1step output #0;
-    output presetn, psel, penable, pwrite, paddr, pwdata;
+    output psel, penable, pwrite, paddr, pwdata;
     input prdata, pready, pslaverr;
   endclocking
   
   clocking mon_cb @(posedge clk);
     default input #1step output #0;
-    input presetn, psel, penable, pwrite, paddr, pwdata, prdata, pready, pslaverr;
+    input psel, penable, pwrite, paddr, pwdata, prdata, pready, pslaverr;
   endclocking
   
-  modport drv_mp (clocking drv_cb);
-  modport mom_mp (clocking mon_cb);
+  modport drv_mp (clocking drv_cb, input pclk, presetn);
+  modport mom_mp (clocking mon_cb, input pclk, presetn);
 endinterface
