f0ae0ecbe27b088a4822049f5e326c7556a633a6
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-16 23:52:22 +0530

    Refactor testbench to use apb_alu_if interface

--- apb_alu/tb_top.sv
+++ apb_alu/tb_top.sv
@@ -8,32 +8,38 @@ module tb_top;
   logic pclk = 0;
   always #5 pclk = ~pclk;
   
-  apb_alu_if vif(pclk);
+  apb_alu_if apb_if(pclk);
   
-  apb_alu_top #(
-    .DW     (DW)
-  ) apb_alu(
+  apb_alu_top #(.DW (DW)) apb_alu(
     .pclk    (pclk),
-    .presetn (vif.resetn),
-    .psel    (vif.w_en),
-    .penable (vif.r_en),
-    .pwrite  (vif.data_in),
-    .paddr   (vif.data_out),
-    .pwdata  (vif.full),
-    .prdata  (vif.empty),
-    .pready  (vif.pready),
-    .pslverr (vif.pslverr)
+    .presetn (presetn),
+    .psel    (apb_if.psel),
+    .penable (apb_if.penable),
+    .pwrite  (apb_if.pwrite),
+    .paddr   (apb_if.paddr),
+    .pwdata  (apb_if.pwdata),
+    .prdata  (apb_if.prdata),
+    .pready  (apb_if.pready),
+    .pslverr (apb_if.pslverr),
   );
   
-  test t;
-  
   initial begin
-    t = new(vif);
+    presetn = 0;
+    repeat(5) @(posedge pclk);
+    presetn = 1;
+  end
+  
+  test t;
+  initial begin
+    t = new(apb_if);
+    
+    wait(presetn == 1);
+    repeat(2) @(posedge pclk);
     t.run();
   end
   
   initial begin
     $dumpfile("testbench.vcd");
-    $dumpvars;
+    $dumpvars(0, tb_top);
   end
 endmodule
