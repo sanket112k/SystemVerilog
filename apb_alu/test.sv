51ea7917245a01abe3aaa60583ac0e35f60a07d0
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-17 23:38:03 +0530

    Refactor test class to use apb_if and add run_random task
    Updated the test class to use apb_if instead of fifo_if and added a new task for random runs.

--- apb_alu/test.sv
+++ apb_alu/test.sv
@@ -2,12 +2,24 @@
 
 class test;
   environment env;
+  virtual apb_if vif;
   
-  function new(virtual fifo_if vif);
+  function new(virtual apb_if vif);
+    this.vif = vif;
     env = new(vif);
   endfunction
   
-  task run;
+  task run_directed();
+    env.agt.gen.directed  = 1;
+    env.agt.gen.num_items = 9;
+    env.run();
+  endtask
+  
+  task run_random(int num = 50, int seed_val = 1, int quota_val = 10);
+    env.agt.gen.directed  = 0;
+    env.agt.gen.num_items = num;
+    env.agt.gen.seed      = seed_val;
+    env.agt.gen.quota     = quota_val;
     env.run();
   endtask
 endclass
