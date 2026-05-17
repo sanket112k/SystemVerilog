d06215c3cdebd4b079d57654fe379a86e5b0d21c
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-17 23:31:38 +0530

    Fix function signatures and variable names in transaction.sv

--- apb_alu/transaction.sv
+++ apb_alu/transaction.sv
@@ -1,3 +1,5 @@
+import tb_pkg::*;
+
 class apb_transaction;
   bit 		   pwrite;
   bit [AW-1:0] paddr;
@@ -5,7 +7,7 @@ class apb_transaction;
   bit [DW-1:0] prdata;
   bit 		   pslverr;
   
-  function void copy(trasaction tr);
+  function void copy(apb_transaction tr);
     paddr   = tr.paddr  ;
     pwrite  = tr.pwrite ;
     pwdata  = tr.pwdata ;
@@ -20,7 +22,7 @@ class alu_in_transaction;
   bit [DW-1:0] op_b  ;
   bit [3:0]    opcode;
   
-  function new(bit [DW-1:0] op_a, bit [DW-1:0] op_b, bit [3:0]    opcode);
+  function new(bit [DW-1:0] op_a = 0, bit [DW-1:0] op_b = 0, bit [3:0] opcode = 0);
     this.op_a   = op_a  ;
     this.op_b   = op_b  ;
     this.opcode = opcode;
@@ -34,19 +36,19 @@ class alu_in_transaction;
 endclass
 
 class alu_out_transaction;
-  bit [DW-1:0] result;
+  bit [DW-1:0] y;
   bit [3:0]    flags;
   bit          opc_illegal;
   
   function new();
-    result      = 0;
+    y           = 0;
     flags       = 0;
-    err_illegal = 0;
+    opc_illegal = 0;
   endfunction
   
   function void copy(alu_out_transaction tr);
-    result      = tr.result     ;
+    y           = tr.y          ;
     flags       = tr.flags      ;
-    err_illegal = tr.err_illegal;
+    opc_illegal = tr.opc_illegal;
   endfunction
 endclass
