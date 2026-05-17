394c56f146cf1d8950453e72148cbde113450fe3
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-17 23:37:20 +0530

    Refactor environment class to add coverage support
    Updated environment class to include coverage handling and modified instantiation of agents and scoreboard.

--- apb_alu/environment.sv
+++ apb_alu/environment.sv
@@ -1,26 +1,38 @@
 `include "transaction.sv"
 `include "agent.sv"
 `include "scoreboard.sv"
+`include "coverage.sv"
 
 class environment;
-  agent agt;
+  agent      agt;
   scoreboard scb;
+  coverage   cov;
   
-  function new(virtual fifo_if vif);
-    agt = new(vif);
-    scb = new(agt.mon2scb);
+  virtual apb_if.drv_mp vif_drv;
+  virtual apb_if.mon_mp vif_mon;
+  
+  function new(virtual apb_if vif);
+    vif_drv = vif.drv_mp;
+    vif_mon = vif.mon_mp;
+    agt = new(vif_drv, vif_mon);
+    scb = new(agt.drv2scb, agt.mon2scb);
+    cov = new(agt.drv2cov);
   endfunction
   
   task run();
+    $display("[ENV] Starting environment");
+    scb.expected_alu_checks = agt.gen.num_items;
     fork
       agt.gen.run();
       agt.drv.run();
       agt.mon.run();
       scb.run();
+      cov.run();
     join_none
     
-    @(scb.done);
+    @(scb.all_done);
+    #100;
     scb.report();
-    $finish;
+    cov.report();
   endtask
 endclass
