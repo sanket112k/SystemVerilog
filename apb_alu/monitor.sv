c281c7aedadbac3d362906836a87a4570c567a20
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-17 23:33:58 +0530

    Update monitor class for new apb_if interface
    Refactor monitor class to use updated interface and simplify transaction handling.

--- apb_alu/monitor.sv
+++ apb_alu/monitor.sv
@@ -1,33 +1,29 @@
 class monitor;
   
-  transaction tr;
-  virtual apb_alu_if vif;
-  mailbox #(transaction) mon2scb;
+  // transaction tr;
+  virtual apb_if.mon_mp vif;
+  mailbox mon2scb;
   
-  function new(virtual apb_alu_if vif, mailbox #(transaction) mon2scb);
+  function new(virtual apb_if.mon_mp vif, mailbox mon2scb);
     this.vif = vif;
     this.mon2scb = mon2scb;
   endfunction
   
   task run();
-    @(vif.mon_cb);
+    apb_transaction tr;
+    $display("[MON] Starting monitor");
     forever begin
-      tr = new();
-      
-      @(vif.mon_cb);
-      
-      tr.presetn  = vif.mon_cb.presetn;
-      tr.psel     = vif.mon_cb.psel;
-      tr.penable  = vif.mon_cb.penable;
-      tr.pwrite   = vif.mon_cb.pwrite;
-      tr.paddr    = vif.mon_cb.paddr;
-      tr.pwdata   = vif.mon_cb.pwdata;
-      tr.prdata   = vif.mon_cb.prdata;
-      tr.pready   = vif.mon_cb.pready;
-      tr.pslverr  = vif.mon_cb.pslverr;
-      
-      mon2scb.put(tr);
-      tr.display("MON");
+      @(posedge vif.pclk);
+      if (vif.mon_cb.psel && vif.mon_cb.penable && vif.mon_cb.pready) begin
+        tr = new();
+        tr.pwrite   = vif.mon_cb.pwrite;
+        tr.paddr    = vif.mon_cb.paddr;
+        tr.pwdata   = vif.mon_cb.pwdata;
+        tr.prdata   = vif.mon_cb.prdata;
+        tr.pslverr  = vif.mon_cb.pslverr;
+        mon2scb.put(tr);
+      end
+      //tr.display("MON");
     end
   endtask
 endclass
