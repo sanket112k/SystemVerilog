ded77b8f099ddff50346c2e4bb4077f4f15a90b9
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-18 22:37:32 +0530

    Refactor tb_top.sv to use apb_if and i2c_if

--- apb_i2c/tb/tb_top.sv
+++ apb_i2c/tb/tb_top.sv
@@ -1,38 +1,20 @@
-//`include "tb_pkg.sv"
+`timescale 1ns/1ps
+
+`include "i2c_master_defines.svh"
+`include "tb_pkg.sv"
 `include "interface.sv"
 `include "test.sv"
 
 module tb_top;
   import tb_pkg::*;
   
-  logic pclk = 0;
-  always #5 pclk = ~pclk;
+  logic pclk;
+  logic presetn;
   
-  apb_i2c_if a2i_if(pclk);
-  
-  apb_i2c DUT #(
-    parameter DW = 32,
-    parameter AW = 8
-  )(
-    .pclk    (pclk),
-    .presetn (presetn),
-    
-    .psel    (a2i_if.w_en),
-    .penable (a2i_if.r_en),
-    .pwrite  (a2i_if.data_in),
-    .paddr   (a2i_if.data_out),
-    .pwdata  (a2i_if.full),
-    .prdata  (a2i_if.empty),
-    .pready  (a2i_if.pready),
-    .pslverr (a2i_if.pslverr),
-    
-    .scl_i	 (a2i_if.scl_i),
-    .scl_o	 (a2i_if.scl_o),
-    .scl_oen (a2i_if.scl_oen),
-    .sda_i	 (a2i_if.sda_i),
-    .sda_o	 (a2i_if.sda_o),
-    .sda_oen (a2i_if.sda_oen),
-  );
+  initial begin
+    pclk = 0;
+    forever #5 pclk = ~pclk;
+  end
   
   initial begin
     presetn = 0;
@@ -40,9 +22,40 @@ module tb_top;
     presetn = 1;
   end
   
+  apb_if apb_if_inst(.pclk(pclk), .presetn(presetn));
+  i2c_if i2c_if_inst();
+  
+  apb_i2c #(
+    .DW(DW),
+    .AW(AW)
+  ) DUT (
+    .pclk    (pclk),
+    .presetn (presetn),
+    
+    .psel    (apb_if_inst.psel),
+    .penable (apb_if_inst.penable),
+    .pwrite  (apb_if_inst.pwrite),
+    .paddr   (apb_if_inst.paddr),
+    .pwdata  (apb_if_inst.pwdata),
+    .prdata  (apb_if_inst.prdata),
+    .pready  (apb_if_inst.pready),
+    .pslverr (apb_if_inst.pslverr),
+    
+    .scl_i	 (i2c_if_inst.scl),
+    .scl_o	 (dut_scl_o),
+    .scl_oen (dut_scl_oen),
+    .sda_i	 (i2c_if_inst.sda),
+    .sda_o	 (dut_sda_o),
+    .sda_oen (dut_sda_oen)
+  );
+  
+  // Connect I2C tristates
+  assign i2c_if_inst.scl = dut_scl_oen ? 1'bz : dut_scl_o;
+  assign i2c_if_inst.sda = dut_sda_oen ? 1'bz : dut_sda_o;
+  
   test t;
   initial begin
-    t = new(a2i_if);
+    t = new(apb_if_inst, i2c_if_inst);
     
     wait(presetn == 1);
     repeat(2) @(posedge pclk);
@@ -51,6 +64,6 @@ module tb_top;
   
   initial begin
     $dumpfile("testbench.vcd");
-    $dumpvars;
+    $dumpvars(0, tb_top);
   end
 endmodule
