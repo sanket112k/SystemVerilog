b62c84ec38068616ef215f6c6c7595de61a233d4
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-05-18 22:39:17 +0530

    Update and rename interface.sv to apb_if.sv

---
+++ apb_i2c/tb/apb_if.sv
@@ -0,0 +1,26 @@
+interface apb_if(input logic pclk, input logic presetn);
+  import tb_pkg::*;
+  
+  logic 		 psel;
+  logic 		 penable;
+  logic 		 pwrite;
+  logic [AW-1:0] paddr;
+  logic [DW-1:0] pwdata;
+  logic [DW-1:0] prdata;
+  logic 		 pready;
+  logic 		 pslverr;
+  
+  clocking drv_cb @(posedge pclk);
+    default input #1step output #0;
+    output psel, penable, pwrite, paddr, pwdata;
+    input prdata, pready, pslverr;
+  endclocking
+  
+  clocking mon_cb @(posedge pclk);
+    default input #1step output #0;
+    input psel, penable, pwrite, paddr, pwdata, prdata, pready, pslverr;
+  endclocking
+  
+  modport drv_mp (clocking drv_cb, input pclk, output presetn);
+  modport mom_mp (clocking mon_cb, input pclk, input presetn);
+endinterface
