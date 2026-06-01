d5d3dff53688678bf5c5157d418f98e4b913e40f
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-06-01 22:59:04 +0530

    Refactor testbench module and improve formatting

--- full_adder_with agent/tb/tb_top.sv
+++ full_adder_with agent/tb/tb_top.sv
@@ -1,29 +1,33 @@
+`timescale 1ns/1ns
+
 `include "interface.sv"
 `include "test.sv"
 
-module tb;
-
-  logic clk;
-
+module tb_top;
+  
+  logic clk = 0;
   always #5 clk = ~clk;
-
+  
   fa_if vif(clk);
 
   full_adder dut(
-    .a(vif.a),
-    .b(vif.b),
-    .cin(vif.cin),
-    .sum(vif.sum),
+    .a   (vif.a),
+    .b   (vif.b),
+    .cin (vif.cin),
+    .sum (vif.sum),
     .cout(vif.cout)
   );
 
   test t;
 
-  initial
-  begin
+  initial begin
     t = new(vif);
     t.run();
-    #100 $finish;
+  end
+  
+  initial begin
+    $dumpfile("testbench.vcd");
+    $dumpvars(0, tb_top);
   end
 
 endmodule
