1d10cf9cc44388347912d13cbf5ce1f3870c95f9
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-06-01 23:01:00 +0530

    Modify generator class to accept additional parameters

--- full_adder_with agent/tb/generator.sv
+++ full_adder_with agent/tb/generator.sv
@@ -2,17 +2,23 @@ class generator;
 
   transaction tr;
   mailbox #(transaction) gen2drv;
+  int num_trans;
+  event scb_done;
 
-  function new(mailbox #(transaction) gen2drv);
-    this.gen2drv = gen2drv;
+  function new(mailbox #(transaction) gen2drv, int num_trans, event scb_done);
+    this.gen2drv   = gen2drv;
+    this.num_trans = num_trans;
+    this.scb_done  = scb_done;
   endfunction
 
   task run();
-    repeat(10) begin
+    repeat(num_trans) begin
       tr = new();
       tr.randomize();
       gen2drv.put(tr);
+      
+      tr.display("GEN");
+      @(scb_done);
     end
   endtask
-
 endclass
