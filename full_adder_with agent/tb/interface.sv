6ff9937560f484aa7a5a82b756bb979016dcb952
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-06-01 22:59:58 +0530

    Enhance interface with clocking blocks and modports
    Added clocking blocks and modports for driver and monitor.

--- full_adder_with agent/tb/interface.sv
+++ full_adder_with agent/tb/interface.sv
@@ -1,9 +1,21 @@
 interface fa_if(input logic clk);
-
   logic a;
   logic b;
   logic cin;
   logic sum;
   logic cout;
-
+  
+  clocking drv_cb @(posedge clk);
+    default input #1step output #0;
+    output a, b, cin;
+    input  sum, cout;
+  endclocking
+  
+  clocking mon_cb @(posedge clk);
+    default input #1step output #0;
+    input a, b, cin, sum, cout;
+  endclocking
+  
+  modport drv_mp (clocking drv_cb, input clk);
+  modport mon_mp (clocking mon_cb, input clk);
 endinterface
