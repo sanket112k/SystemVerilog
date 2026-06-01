e5d224292b408908970a31e8065446b88e0c5f25
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-06-01 23:01:35 +0530

    Add event parameter to constructor and update run task
    Updated the constructor to include an event parameter and modified the run task to signal completion.

--- full_adder_with agent/tb/driver.sv
+++ full_adder_with agent/tb/driver.sv
@@ -3,21 +3,25 @@ class driver;
   virtual fa_if vif;
   mailbox #(transaction) gen2drv;
   transaction tr;
+  event drv_done;
 
-  function new(virtual fa_if vif, mailbox #(transaction) gen2drv);
-    this.vif = vif;
-    this.gen2drv = gen2drv;
+  function new(virtual fa_if vif, mailbox #(transaction) gen2drv, event drv_done);
+    this.vif      = vif;
+    this.gen2drv  = gen2drv;
+    this.drv_done = drv_done;
   endfunction
 
   task run();
     forever begin
       gen2drv.get(tr);
+      @(vif.drv_cb);
+      
+      vif.drv_cb.a   <= tr.a;
+      vif.drv_cb.b   <= tr.b;
+      vif.drv_cb.cin <= tr.cin;
 
-      vif.a   <= tr.a;
-      vif.b   <= tr.b;
-      vif.cin <= tr.cin;
-
-      #1;
+      tr.display("DRV");
+      -> drv_done;
     end
   endtask
 
