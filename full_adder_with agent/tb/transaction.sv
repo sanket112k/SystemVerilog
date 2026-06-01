a32e9dae08d3dd039b0577a1cfaa3f59fb9fa837
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-06-01 23:00:25 +0530

    Enhance transaction class with sum, cout, and display
    Added sum and cout variables along with a display function to the transaction class.

--- full_adder_with agent/tb/transaction.sv
+++ full_adder_with agent/tb/transaction.sv
@@ -1,10 +1,11 @@
 class transaction;
-
   rand bit a;
   rand bit b;
   rand bit cin;
-
-  bit sum;
-  bit cout;
-
+       bit sum;
+       bit cout;
+  
+  function void display(string name);
+    $display("@t=%0t [%s] a=%b b=%b cin=%b sum=%b cout=%b", $time, name, a, b, cin, sum, cout);
+  endfunction
 endclass
