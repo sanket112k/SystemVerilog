4ea943fdc362fa7a24c43461fc3d14cd70168750
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-06-01 23:02:50 +0530

    Enhance scoreboard class with new events and report method
    Updated the scoreboard class to include additional event handling and reporting functionality.

--- full_adder_with agent/tb/scoreboard.sv
+++ full_adder_with agent/tb/scoreboard.sv
@@ -2,9 +2,15 @@ class scoreboard;
 
   mailbox #(transaction) mon2scb;
   transaction tr;
+  event scb_done;
+  event test_done;
+  int pass, fail;
+  int num_trans;
 
-  function new(mailbox #(transaction) mon2scb);
-    this.mon2scb = mon2scb;
+  function new(mailbox #(transaction) mon2scb, int num_trans, event scb_done);
+    this.mon2scb   = mon2scb;
+    this.num_trans = num_trans;
+    this.scb_done  = scb_done;
   endfunction
 
   task run();
@@ -16,12 +22,29 @@ class scoreboard;
 
       exp_sum  = tr.a ^ tr.b ^ tr.cin;
       exp_cout = (tr.a & tr.b) | (tr.b & tr.cin) | (tr.a & tr.cin);
-
-      if(tr.sum == exp_sum && tr.cout == exp_cout)
-        $display("PASS");
-      else
-        $display("FAIL");
+      
+      tr.display("SCB");
+      
+      if(tr.sum == exp_sum && tr.cout == exp_cout) begin
+        pass++;
+        $display("@t=%0t [SCB] {PASS = %0d}", $time, pass);
+      end else begin
+        fail++;
+        $display("@t=%0t [SCB] {FAIL = %0d} exp_sum=%0b exp_cout=%0b", $time, fail, exp_sum, exp_cout);
+      end
+      
+      $display("============================");
+      
+      -> scb_done;
+      if(pass + fail == num_trans)
+        -> test_done;
     end
   endtask
-
+  
+  function void report();
+    $display("=========== REPORT ===========");
+    $display("PASS = %0d", pass);
+    $display("FAIL = %0d", fail);
+    $display("==============================");
+  endfunction
 endclass
