0fb4dc7cb2dff6b14e2f86399ceeab597f192ac1
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-06-01 23:05:18 +0530

    Update agent class with transaction count and constructors
    Modified the agent class to include a transaction count and updated constructor parameters for generator, driver, and monitor.

--- full_adder_with agent/tb/agent.sv
+++ full_adder_with agent/tb/agent.sv
@@ -3,22 +3,25 @@
 `include "monitor.sv"
 
 class agent;
-
+  
+  int num_trans = 10;
   generator gen;
   driver drv;
   monitor mon;
+  event drv_done, scb_done;
 
   mailbox #(transaction) gen2drv;
   mailbox #(transaction) mon2scb;
 
-  function new(virtual fa_if vif);
+  function new(virtual fa_if vif, event scb_done);
 
-    gen2drv = new();
-    mon2scb = new();
+    gen2drv = new(1);
+    mon2scb = new(1);
 
-    gen = new(gen2drv);
-    drv = new(vif, gen2drv);
-    mon = new(vif, mon2scb);
+    gen = new(gen2drv, num_trans, scb_done);
+    drv = new(vif, gen2drv, drv_done);
+    mon = new(vif, mon2scb, drv_done);
+    this.scb_done = scb_done;
 
   endfunction
 
