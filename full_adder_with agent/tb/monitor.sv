33f01fe22a3b81dd162b3041133eff587c695099
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-06-01 23:02:09 +0530

    Add drv_done event to monitor class
    Modified constructor to include drv_done event and updated run task to use it.

--- full_adder_with agent/tb/monitor.sv
+++ full_adder_with agent/tb/monitor.sv
@@ -3,25 +3,30 @@ class monitor;
   virtual fa_if vif;
   mailbox #(transaction) mon2scb;
   transaction tr;
+  event drv_done;
 
-  function new(virtual fa_if vif, mailbox #(transaction) mon2scb);
-    this.vif = vif;
-    this.mon2scb = mon2scb;
+  function new(virtual fa_if vif, mailbox #(transaction) mon2scb, event drv_done);
+    this.vif      = vif;
+    this.mon2scb  = mon2scb;
+    this.drv_done = drv_done;
   endfunction
 
   task run();
+    @(vif.mon_cb);
     forever begin
       tr = new();
+      
+      @(vif.mon_cb);
 
-      #1;
-
-      tr.a   = vif.a;
-      tr.b   = vif.b;
-      tr.cin = vif.cin;
-      tr.sum = vif.sum;
-      tr.cout= vif.cout;
+      tr.a   = vif.mon_cb.a;
+      tr.b   = vif.mon_cb.b;
+      tr.cin = vif.mon_cb.cin;
+      tr.sum = vif.mon_cb.sum;
+      tr.cout= vif.mon_cb.cout;
 
       mon2scb.put(tr);
+      tr.display("MON");
+      @(drv_done);
     end
   endtask
 
